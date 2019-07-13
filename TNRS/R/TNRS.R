94afe4d0166c5279367989c96e89c03a00d864f5
bmaitner <bmaitner@gmail.com>
2019-07-13 11:31:59 -0600

    initial draft TNRS function

---
+++ TNRS/R/TNRS.R
@@ -0,0 +1,65 @@
+#'Resolve (plant) taxonomic names
+#'
+#'TNRS taxonomically resolved plant taxonomic names.
+#' @param taxonomic_names 
+#' @param sources
+#' @return Dataframe containing TNRS results.
+#' @import RCurl jsonlite
+#' @export
+#' @examples \dontrun{
+#' fulldata <- 
+#' read.csv("http://bien.nceas.ucsb.edu/bien/wp-content/uploads/2019/07/tnrs_testfile.csv", header=FALSE)
+#'fulldata$V2 <-gsub(pattern = "'",replacement = "",x = fulldata$V2) #Currently we have an issue with cultivars, so this is a workaround
+#'
+#'results <- TNRS(taxonomic_names = fulldata)
+#'   
+#' # Inspect the results
+#' head(results, 10)
+#' }
+#' 
+TNRS <- function(taxonomic_names,
+                 sources = "tpl,gcc,ildis,tropicos,usda",
+                 classification = "tropicos",
+                 mode = "resolve"
+                 ){
+  
+  # URL for GNRS API
+  url = "https://tnrsapidev.xyz/tnrs_api.php"
+  
+  # Convert the data to JSON
+  data_json <- jsonlite::toJSON(unname(taxonomic_names))
+  
+  # Convert the options to data frame and then JSON
+  opts <- data.frame(c(sources),c(classification), c(mode))
+  names(opts) <- c("sources", "class", "mode")
+  opts_json <- jsonlite::toJSON(opts)
+  opts_json <- gsub('\\[','',opts_json)
+  opts_json <- gsub('\\]','',opts_json)
+  
+  # Combine the options and data into single JSON object
+  input_json <- paste0('{"opts":', opts_json, ',"data":', data_json, '}' )
+  
+  # Construct the request
+  headers <- list('Accept' = 'application/json', 'Content-Type' = 'application/json', 'charset' = 'UTF-8')
+  
+  # Send the API request
+  results_json <- RCurl::postForm(url, .opts=list(postfields= input_json, httpheader=headers))
+
+  #clean json results to prevent encoding issues
+  
+
+  # Convert JSON file to a data frame
+  results <- jsonlite::fromJSON(results_json)
+
+  #Clean up results
+
+  results<-gsub(pattern = '"',replacement = "",x = results) #remove quotation marks used by the API
+  results<-as.data.frame(results,stringsAsFactors = F) #convert to data.frame
+  colnames(results) <- as.character(results[1,]) #add column names
+  results <- results[-1,] #remove first row (which contained column names)
+  rownames(results) <- NULL	# Reset row numbers
+  
+  return(results)
+  
+}
+  
---
+++ TNRS/R/description.R
@@ -0,0 +1,12 @@
+#' TNRS: Tools for accessing the Taxonomic Name Resolution Service (TNRS).
+#'
+#' @description The Taxonomic Name Resolution Service (TNRS) R package provides access to the TNRS as well as other associated tools.
+#' 
+#' @section Getting started:
+#' Type vignette("TNRS") to view the vignette, which contains useful information on the TNRS package.
+#' 
+#' @references Boyle, B., Hopkins, N., Lu, Z., Garay, J. A. R., Mozzherin, D., Rees, T., ... & Lowry, S. (2013). The taxonomic name resolution service: an online tool for automated standardization of plant names. BMC bioinformatics, 14(1), 16.
+#' 
+#' @docType package
+#' @name TNRS
+#' @aliases TNRS-package
