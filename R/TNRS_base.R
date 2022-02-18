a81d80fb92e9cabf8aec7e7e5d0a21a4ed9b5405
Brian <bmaitner@gmail.com>
2022-02-17 22:02:34 -0500

    renamed .TNRS_base to TNRS_base

--- R/TNRS_base.R
+++ R/TNRS_base.R
@@ -1,4 +1,4 @@
-#'Resolve a single batch of (plant) taxonomic names
+'Resolve a single batch of (plant) taxonomic names
 #'
 #'Resolve a small batch of plant taxonomic names
 #' @param taxonomic_names Data.frame containing two columns: 1) Row number, 2) Taxonomic names to be resolved (or parsed).  Alternatively, a character vector of names can be supplied.  
@@ -14,91 +14,49 @@
 #' @importFrom jsonlite toJSON fromJSON
 #' @keywords Internal
 #' 
-.TNRS_base <- function(taxonomic_names,
+TNRS_base <- function(taxonomic_names,
                        sources = c("tropicos","wcvp"),
                        classification = "tropicos",
                        mode = "resolve",
                        matches = "best",
-                       accuracy = NULL
-){
+                       accuracy = NULL,
+                       ...
+                       ){
   
-  #reformat sources if need  be (only likely when using internal function externally)
-  if(length(sources)>1) {
-    sources <- paste0(sources, collapse = ",")
-  }
-  
-  
+
   # Check for internet access
-  if (!check_internet()) {
-    message("This function requires internet access, please check your connection.")
-    return(invisible(NULL))
-  }
-
-  # URL for TNRS API
-    #url = "https://tnrsapidev.xyz/tnrs_api.php"
-    url = "http://vegbiendev.nceas.ucsb.edu:8975/tnrs_api.php" # Dev (vegbiendev)
-    #url = http://vegbiendev.nceas.ucsb.edu:9975/tnrs_api.php #public development
-    #url = "https://tnrsapi.xyz/tnrs_api.php" #production
-
-  
-  # If taxonomic names are supplied as a character string, make them into a data.frame
-  
-  if(class(taxonomic_names)=="character"){
-    taxonomic_names <- data.frame(ID = 1:length(taxonomic_names), taxonomic_names)
-  }
-  
-  #Check that accuracy makes sense
-
-  if(!class(accuracy) %in% c("NULL", "numeric")) {
-    stop("accuracy should be either numeric between 0 and 1, or NULL")
+    if (!check_internet()) {
+      message("This function requires internet access, please check your connection.")
+      return(invisible(NULL))
     }
   
-  
-  # Convert the data to JSON
-  data_json <- jsonlite::toJSON(unname(taxonomic_names))
-  
-  # Convert the options to data frame and then JSON
-  
-  if (is.null(accuracy)) {
-    
-    opts <- data.frame(c(sources),c(classification), c(mode), c(matches))
-    names(opts) <- c("sources", "class", "mode", "matches")
-      
-  }else{
-    opts <- data.frame(c(sources),c(classification), c(mode), c(matches), c(accuracy))
-    names(opts) <- c("sources", "class", "mode", "matches", "acc")
-    
-    
-  }
 
-  opts_json <-  jsonlite::toJSON(opts)
-  opts_json <- gsub('\\[','',opts_json)
-  opts_json <- gsub('\\]','',opts_json)
-  
-  # Combine the options and data into single JSON object
-  input_json <- paste0('{"opts":', opts_json, ',"data":', data_json, '}' )
-  
-  # Send the API request
-  # Send the request in a "graceful failure" wrapper for CRAN compliance
-  tryCatch(expr = results_json <- POST(url = url,
-                                       add_headers('Content-Type' = 'application/json'),
-                                       add_headers('Accept' = 'application/json'),
-                                       add_headers('charset' = 'UTF-8'),
-                                       body = input_json,
-                                       encode = "json"),
-           error = function(e) {
-             message("There appears to be a problem reaching the API.")
-           })
-  
-  #Return NULL if API isn't working
-  if(!exists("results_json")){return(invisible(NULL))}
-  
-  # Convert JSON results to a data frame
-  results <- fromJSON(rawToChar(results_json$content)) 
-  #results <- as.data.frame(results)
-  
-  
-  return(results)
-  
+  # If taxonomic names are supplied as a character string, make them into a data.frame
+    
+    if(class(taxonomic_names)=="character"){
+      taxonomic_names <- data.frame(ID = 1:length(taxonomic_names), taxonomic_names)
+    }
+
+  # Check that accuracy makes sense
+
+    if(!class(accuracy) %in% c("NULL", "numeric")) {
+      stop("accuracy should be either numeric between 0 and 1, or NULL")
+    }
+
+  # Convert the data to JSON
+    data_json <- jsonlite::toJSON(unname(taxonomic_names))
+
+  # Send data to core function
+    results <- TNRS_core(data_json = data_json,
+                         sources = sources,
+                         classification = classification,
+                         mode = mode,
+                         matches = matches,
+                         accuracy = accuracy,
+                         ...)
+
+  # Return results
+    return(results)
+
 }
-  
+
