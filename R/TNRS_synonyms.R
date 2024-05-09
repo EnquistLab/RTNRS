c9ad42269f606886cddf0bde5f13fe2f457726da
Brian <bmaitner@gmail.com>
2024-05-09 11:17:38 -0400

    added TNRS_synonyms function

---
+++ R/TNRS_synonyms.R
@@ -0,0 +1,71 @@
+"Get synonyms for a single (plant) taxonomic name
+#"
+#' Get synonyms for a single species
+#' @param taxonomic_name Data.frame containing a single row and two columns: 1) Row number, 2) Taxonomic name to get synonyms of. Alternatively, a character vector of names can be supplied.
+#' @param source Character. Taxonomic source (1) to use. Default is "wcvp". Options include "wfo", and "wcvp".
+#' @param skip_internet_check Should the check for internet connectivity be skipped? Default is FALSE.
+#' @param ... Additional parameters passed to internal functions
+#' @return Dataframe containing TNRS results.
+#' @note This function only handles a single source and a single taxonomic name at a time. This is by design.
+#' @note wfo = World Flora Online, wcvp = World Checklist of Vascular Plants.
+#' @import httr
+#' @export
+#' @importFrom jsonlite toJSON fromJSON
+#' @examples{
+#'
+#' TNRS_synonyms(taxonomic_names = "Palicourea elata")
+#'
+#' }
+TNRS_synonyms <- function(taxonomic_names,
+                          source = "wcvp",
+                          skip_internet_check = FALSE,
+                          ...) {
+  # Check for internet access
+
+  if (!skip_internet_check) {
+    if (!check_internet()) {
+      message("This function requires internet access, please check your connection.")
+      return(invisible(NULL))
+    }
+  }
+
+  # If taxonomic names are supplied as a character string, make them into a data.frame
+
+  if (inherits(taxonomic_names, "character")) {
+    taxonomic_names <- data.frame(ID = 1:length(taxonomic_names), taxonomic_names)
+  }
+
+  # Make sure only one name is supplied
+
+  if (nrow(taxonomic_names) != 1) {
+    stop("This function can only take one taxonomic name at a time.")
+  }
+
+  # Make sure only one source is supplied
+
+  if (length(source) != 1) {
+    stop("This function requires one (and only one) source be specified.")
+  }
+
+
+
+  # Convert the data to JSON
+
+  data_json <- jsonlite::toJSON(unname(taxonomic_names))
+
+  # Send data to core function
+
+  results <- TNRS_core(
+    data_json = data_json,
+    sources = source,
+    classification = NA,
+    mode = "syn",
+    matches = NA,
+    accuracy = NA,
+    ...
+  )
+
+  # Return results
+
+  return(results)
+}
